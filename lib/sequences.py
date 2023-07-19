53a96ebd1d3028d9b710fbfffbe9305d3b512697
AldenReyes <122179486+AldenReyes@users.noreply.github.com>
2023-07-18 19:03:22 -0700

    Completes function that prints out the fibonacci sequence.

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    pass
+    counter = length - 2
+    fibonacci_list = list([0, 1])
+    if counter < 0:
+        for i in range(abs(counter)):
+            fibonacci_list.pop()
+    while counter > 0:
+        fibonacci_list.append(fibonacci_list[-2] + fibonacci_list[-1])
+        counter -= 1
+    print(fibonacci_list)
+
+
+print_fibonacci(9)
